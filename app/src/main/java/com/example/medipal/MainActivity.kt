a226b251d459191af8d9c3fba70bd9401ebc31ae
sahil <mrbeast6868@gmail.com>
2024-12-27 17:24:32 +0530

    add themes and colours

--- app/src/main/java/com/example/medipal/MainActivity.kt
+++ app/src/main/java/com/example/medipal/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.medipal.ui.theme.MedipalTheme
+import com.example.compose.MedipalTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
