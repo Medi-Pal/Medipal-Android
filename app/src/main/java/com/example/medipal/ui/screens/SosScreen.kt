73a4a718198631232359aac12215785ae84aa2cc
sahil <mrbeast6868@gmail.com>
2025-04-07 04:13:27 +0530

    Added add icon

--- app/src/main/java/com/example/medipal/ui/screens/SosScreen.kt
+++ app/src/main/java/com/example/medipal/ui/screens/SosScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -54,7 +55,11 @@ fun SosScreen(
             FloatingActionButton(
                 onClick = { showAddDialog = true }
             ) {
-                Text("+")
+                Icon(
+                    Icons.Default.Add,
+                    contentDescription = "Add Contact",
+                    modifier = Modifier
+                )
             }
         },
         modifier = modifier
